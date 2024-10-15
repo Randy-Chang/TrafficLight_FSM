935cd81b75fb6a0755ac87093c6704e43c25d34b
育郢 張 <gdba5712@gmail.com>
2024-10-16 00:45:38 +0800

    初版紅綠燈

--- TrafficLight_FSM/Form1.Designer.cs
+++ TrafficLight_FSM/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pbRed = new PictureBox();
+            pbYellow = new PictureBox();
+            pbGreen = new PictureBox();
+            gbLight = new GroupBox();
+            btnStart = new Button();
+            btnStop = new Button();
+            ((System.ComponentModel.ISupportInitialize)pbRed).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbYellow).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbGreen).BeginInit();
+            gbLight.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pbRed
+            // 
+            pbRed.BackColor = Color.Black;
+            pbRed.Location = new Point(6, 31);
+            pbRed.Name = "pbRed";
+            pbRed.Size = new Size(80, 80);
+            pbRed.TabIndex = 0;
+            pbRed.TabStop = false;
+            // 
+            // pbYellow
+            // 
+            pbYellow.BackColor = Color.Black;
+            pbYellow.Location = new Point(92, 31);
+            pbYellow.Name = "pbYellow";
+            pbYellow.Size = new Size(80, 80);
+            pbYellow.TabIndex = 1;
+            pbYellow.TabStop = false;
+            // 
+            // pbGreen
+            // 
+            pbGreen.BackColor = Color.Black;
+            pbGreen.Location = new Point(178, 31);
+            pbGreen.Name = "pbGreen";
+            pbGreen.Size = new Size(80, 80);
+            pbGreen.TabIndex = 2;
+            pbGreen.TabStop = false;
+            // 
+            // gbLight
+            // 
+            gbLight.BackColor = Color.FromArgb(192, 255, 255);
+            gbLight.Controls.Add(pbRed);
+            gbLight.Controls.Add(pbGreen);
+            gbLight.Controls.Add(pbYellow);
+            gbLight.Location = new Point(12, 12);
+            gbLight.Name = "gbLight";
+            gbLight.Size = new Size(267, 121);
+            gbLight.TabIndex = 3;
+            gbLight.TabStop = false;
+            gbLight.Text = "Light";
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(14, 149);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(124, 65);
+            btnStart.TabIndex = 4;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(155, 149);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(124, 65);
+            btnStop.TabIndex = 5;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(471, 384);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Controls.Add(gbLight);
+            Font = new Font("Microsoft JhengHei UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            Margin = new Padding(5);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pbRed).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbYellow).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbGreen).EndInit();
+            gbLight.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pbRed;
+        private PictureBox pbYellow;
+        private PictureBox pbGreen;
+        private GroupBox gbLight;
+        private Button btnStart;
+        private Button btnStop;
     }
 }
--- TrafficLight_FSM/Form1.cs
+++ TrafficLight_FSM/Form1.cs
@@ -1,10 +1,143 @@
+using System.Diagnostics;
+using System.Threading;
+
 namespace TrafficLight_FSM
 {
     public partial class Form1 : Form
     {
+        TrafficLight trafficLight;
+        
+
         public Form1()
         {
             InitializeComponent();
+
+            trafficLight = new TrafficLight(pbRed, pbYellow, pbGreen);
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            trafficLight.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            trafficLight.Stop();
+        }
+    }
+
+    enum ETrafficLightState { Idle, Red, Green, Yellow }
+
+    class TrafficLight
+    {
+        ETrafficLightState stateNow;
+        PictureBox pbRed, pbYellow, pbGreen;
+        Stopwatch stopwatch;
+        Thread thread;
+        ManualResetEvent mre;
+        bool IsFirst = false;
+
+        public TrafficLight(PictureBox pbRed, PictureBox pbYellow, PictureBox pbGreen)
+        {
+            this.pbRed = pbRed;
+            this.pbYellow = pbYellow;
+            this.pbGreen = pbGreen;
+
+            stateNow = ETrafficLightState.Idle;
+
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            thread = new Thread(Run);
+            thread.IsBackground = true;
+            thread.Start();
+
+            // ManualResetEvent 可以在初始化時設定為 true 或 false，分別代表已被觸發或尚未觸發的狀態。
+            mre = new ManualResetEvent(false);
+            // 將 ManualResetEvent 重置為未觸發狀態，使後續的線程再次被阻塞。
+            mre.Reset();
+        }
+
+        public void Start()
+        {
+            stateNow = ETrafficLightState.Red;
+            IsFirst = true;
+
+            //觸發 ManualResetEvent，讓所有等待的線程繼續執行。
+            mre.Set();
+        }
+
+        public void Stop()
+        {
+            // 將 ManualResetEvent 重置為未觸發狀態，使後續的線程再次被阻塞。
+            mre.Reset();
+        }
+
+        void Run()
+        {
+            while (true)
+            {
+                mre.WaitOne();
+
+                switch (stateNow)
+                {
+                    case ETrafficLightState.Red:
+                        {
+                            if (IsFirst)
+                            {
+                                pbRed.BackColor = Color.Red;
+                                pbGreen.BackColor = Color.Black;
+                                pbYellow.BackColor = Color.Black;
+
+                                stopwatch.Restart();
+                                stateNow = ETrafficLightState.Green;
+
+                                IsFirst = false;
+                            }
+                            else if (stopwatch.Elapsed.TotalSeconds >= 2)
+                            {
+                                pbRed.BackColor = Color.Red;
+                                pbGreen.BackColor = Color.Black;
+                                pbYellow.BackColor = Color.Black;
+
+                                stopwatch.Restart();
+                                stateNow = ETrafficLightState.Green;
+                            }
+                        }
+                        break;
+
+                    case ETrafficLightState.Green:
+                        {
+                            if (stopwatch.Elapsed.TotalSeconds >= 2)
+                            {
+                                pbRed.BackColor = Color.Black;
+                                pbGreen.BackColor = Color.Green;
+                                pbYellow.BackColor = Color.Black;
+
+                                stopwatch.Restart();
+                                stateNow = ETrafficLightState.Yellow;
+                            }
+                        }
+                        break;
+
+                    case ETrafficLightState.Yellow:
+                        {
+                            if (stopwatch.Elapsed.TotalSeconds >= 2)
+                            {
+                                pbRed.BackColor = Color.Black;
+                                pbGreen.BackColor = Color.Black;
+                                pbYellow.BackColor = Color.Yellow;
+
+                                stopwatch.Restart();
+                                stateNow = ETrafficLightState.Red;
+                            }
+                        }
+                        break;
+                }
+
+            }
+
         }
     }
 }
